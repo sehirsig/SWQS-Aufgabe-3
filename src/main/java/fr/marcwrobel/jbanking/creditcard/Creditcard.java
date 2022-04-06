60c54ce46019afe1413226887e19cd0beb7a97da
sehirsig <sebastianhirsig@bluewin.ch>
2022-04-06 15:52:41 +0200

    Fixed getCreditCardGroup

--- src/main/java/fr/marcwrobel/jbanking/creditcard/Creditcard.java
+++ src/main/java/fr/marcwrobel/jbanking/creditcard/Creditcard.java
@@ -32,26 +32,29 @@ public class Creditcard {
   public static String getCreditCardGroup(String ccNumber) {
     String card = ccNumber;
 
-    String regex = "^(?:(?<visa>4[0-9]{12}(?:[0-9]{3})?)|" +
-      "(?<mastercard>5[1-5][0-9]{14})|" +
-      "(?<discover>6(?:011|5[0-9]{2})[0-9]{12})|" +
-      "(?<amex>3[47][0-9]{13})|" +
-      "(?<diners>3(?:0[0-5]|[68][0-9])?[0-9]{11})|" +
-      "(?<jcb>(?:2131|1800|35[0-9]{3})[0-9]{11}))$";
+    String visaRegex = "^(?:(?<visa>4[0-9]{12}(?:[0-9]{3})?))$";
+    String mcRegex = "^(?:(?<mastercard>5[1-5][0-9]{14}))$";
+    String discRegex = "^(?:(?<discover>6(?:011|5[0-9]{2})[0-9]{12}))$";
+    String amexRegex = "^(?:(?<amex>3[47][0-9]{13}))$";
+    String dinersRegex = "^(?:(?<diners>3(?:0[0-5]|[68][0-9])?[0-9]{11}))$";
+    String jcbRegex = "^(?:(?<jcb>(?:2131|1800|35[0-9]{3})[0-9]{11}))$";
 
-    Pattern pattern = Pattern.compile(regex);
-    card = card.replaceAll("-", "");
-    Matcher matcher = pattern.matcher(card);
-
+    String[] regex = {visaRegex, mcRegex, discRegex, amexRegex, dinersRegex, jcbRegex};
     String[] anbieter = {"visa", "mastercard", "discover", "amex", "diners", "jcb"};
 
-    if(matcher.matches()) {
-      for (String anb : anbieter) {
-        if (anb != null) {
-          return anb;
-        }
+    int counter = 0;
+    card = card.replaceAll("-", "");
+
+    for (String reg: regex) {
+      String anb = anbieter[counter];
+      Pattern pattern = Pattern.compile(reg);
+      Matcher matcher = pattern.matcher(card);
+      if(matcher.matches()) {
+        return anb;
       }
+      counter++;
     }
+
     return "ERROR!";
   }
 
